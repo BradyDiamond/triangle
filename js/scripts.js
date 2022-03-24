7b8f6eeec048267c34058cacb338b9a7bb8e9d90
Brady Diamond <brdydmnd@gmail.com>
2022-03-24 15:04:40 -0700

    wip troubleshoot
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-// BUSINESS LOGIC
-
-function fullName(firstName, lastName) {
-  return firstName + lastName;
-}
-
-// UI LOGIC 
-
 $(document).ready(function() {
-  $("form#name").submit(function(event) {
+  console.log("script executing!");
+  $("form#triangle").submit(function(event) {
+    console.log("agin,executing");
     event.preventDefault();
-    const firstVal = $("#val1").val();
-    const secondVal = $("#val2").val();
-    const thirdVal = $("#val3").val();
-
-    const result = (firstVal + secondVal + LastVal);
-    const receiptText = " Thank you for your Purchase!"
-    $("#output").text(result + " - " + flavor.toUpperCase() + " " + receiptText);
-    $("#output").show();
+    const firstVal = parseInt($("#val1").val());
+    const secondVal = parseInt($("#val2").val());
+    //const thirdVal = parseInt($("#val3").val());
+    if (firstVal === secondVal) {
+      $('#Equilateral').show();
+    }  
+   // if (firstVal === secondVal && firstVal === thirdVal && secondVal === thirdVal) {
+    //         $('#Equilateral').show();
+    //         }
+    //         else if (firstVal === secondVal || firstVal === thirdVal || secondVal === thirdVal) {
+    //         $('#Isoscele').show();
+    //         }
+    //         else if (firstVal !== secondVal && firstVal !== thirdVal && secondVal !== thirdVal) {
+    //           $('#Scalene').show();
+    //         }  
+    //         else {
+    //           alert("NOT a triangle");
+    //         }
   });
 });
+
+
+
+
+
+ 
