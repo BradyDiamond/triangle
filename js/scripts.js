7600c3e0230a436cc94e1691057e6ba01f940392
Brady Diamond <brdydmnd@gmail.com>
2022-03-24 15:49:50 -0700

    implimen js functionality
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
 $(document).ready(function() {
-  console.log("script executing!");
-  $("form#triangle").submit(function(event) {
-    console.log("agin,executing");
-    event.preventDefault();
+    $("form#triangle").submit(function(event) {
+       event.preventDefault();
     const firstVal = parseInt($("#val1").val());
     const secondVal = parseInt($("#val2").val());
-    //const thirdVal = parseInt($("#val3").val());
-    if (firstVal === secondVal) {
+    const thirdVal = parseInt($("#val3").val());
+
+    if (firstVal + secondVal <= thirdVal)  {
+      return alert("NOT a triangle");
+    } else if (secondVal + thirdVal <= firstVal) {
+      return alert("NOT a triangle");
+    } else if (firstVal + thirdVal <= secondVal) {
+      return alert("NOT a triangle");
+    } 
+      if (firstVal === secondVal && firstVal === thirdVal) {
       $('#Equilateral').show();
-    }  
-   // if (firstVal === secondVal && firstVal === thirdVal && secondVal === thirdVal) {
-    //         $('#Equilateral').show();
-    //         }
-    //         else if (firstVal === secondVal || firstVal === thirdVal || secondVal === thirdVal) {
-    //         $('#Isoscele').show();
-    //         }
-    //         else if (firstVal !== secondVal && firstVal !== thirdVal && secondVal !== thirdVal) {
-    //           $('#Scalene').show();
-    //         }  
-    //         else {
-    //           alert("NOT a triangle");
-    //         }
+      } else if (firstVal === secondVal || firstVal === thirdVal) {
+      $('#Isosceles').show();
+      } else if (secondVal === thirdVal) {
+      $('#Isosceles').show();
+      } else if (firstVal !== secondVal && firstVal !== thirdVal && secondVal !== thirdVal) {
+      $('#Scalene').show();
+    
+      }
   });
 });
 
 
 
 
-
- 
